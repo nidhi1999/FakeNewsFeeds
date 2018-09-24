47452bcd93ec2a351b86f5430efde3328180c563
nidhi1999 <nidfhi@gmail.com>
2018-09-24 20:44:32 +0530

    application of lstm on fake news feeds

---
+++ lstm.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 21 22:42:30 2018
+
+@author: Nidhi
+
+"""
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import tensorflow as tf #magical gradient computation
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import nltk
+from nltk.corpus import stopwords
+import  json
+nltk.download('punkt')
+
+
+heading,body,label=[],[],[]
+for i in range(1,121):
+    
+   path="C:/Users/Nidhi/Desktop/FakeNewsNet-master/FakeNewsNet-master/Data/PolitiFact/FakeNewsContent/PolitiFact_Fake_"+str(i)+"-Webpage.json"
+
+   fake_file=json.loads(open(path).read())
+   
+   body.append(fake_file['text'])
+   heading.append(fake_file['title'])
+   label.append(0)
+   
+
+for i in range(1,121):
+    
+   path="C:/Users/Nidhi/Desktop/FakeNewsNet-master/FakeNewsNet-master/Data/PolitiFact/RealNewsContent/PolitiFact_Real_"+str(i)+"-Webpage.json"
+
+   Real_file=json.loads(open(path).read())
+   
+   body.append(Real_file['text'])
+   heading.append(Real_file['title'])
+   label.append(1)
+   
+df=pd.DataFrame()
+df['heading']=heading
+df['body']=body
+df['label']=label
+df['length']=[len(heading) for heading in df['heading']]
+df['length'].describe()
+real_text = ' '.join(df[df['label'] == 1]['heading'])
+fake_text = ' '.join(df[df['label'] == 0]['heading'])
+fake_words = [word for word in nltk.tokenize.word_tokenize(fake_text) if word not in stopwords.words('english') and len(word) > 3]
+real_words = [word for word in nltk.tokenize.word_tokenize(real_text) if word not in stopwords.words('english') and len(word) > 3]
+
+common_fake = nltk.FreqDist(fake_words).most_common(25)
+common_real =nltk.FreqDist(real_words).most_common(25)
+fake_ranks = []
+fake_counts = []
+real_ranks = []
+real_counts = []
+
+for ii, word in enumerate(reversed(common_fake)):
+    fake_ranks.append(ii)
+    fake_counts.append(word[1])
+
+for ii, word in enumerate(reversed(common_real)):
+    real_ranks.append(ii)
+    real_counts.append(word[1])
+
+plt.figure(figsize=(20, 7))
+
+plt.scatter(fake_ranks, fake_counts)
+
+for labels, fake_rank, fake_count in zip(common_fake, fake_ranks, fake_counts):
+    plt.annotate(
+        labels[0],
+        xy = (fake_rank, fake_count)
+    )
+    
+    plt.scatter(real_ranks, real_counts)
+plt.title('Real vs Fake Headlines')
+
+for labels, real_rank, real_count in zip(common_real, real_ranks, real_counts):
+    plt.annotate(
+        labels[0],
+        xy = (real_rank, real_count)
+    )
+    
+real_patch = mpatches.Patch(color='orange', label='Real')
+fake_patch = mpatches.Patch(color='blue', label='Fake')
+plt.legend(handles=[real_patch, fake_patch])
+
+plt.show()
+def pad(x):
+    
+    if len(x) < 69:
+        
+        return x + ' ' * (69 - len(x))
+    
+    return x
+
+def trim(x):
+    
+    if len(x) > 69:
+        
+        return x[:69]
+    
+    return x
+
+df['heading'] = df['heading'].apply(pad)
+df['heading'] = df['heading'].apply(trim)
+df['length'] = [len(heading) for heading in df['heading']]
+df.describe()
+text = ' '.join(df['heading'])
+dictionary_size = len(set(text))
+dictionary = sorted(set(text))
+character_map = { k:v for v, k in enumerate(dictionary) }
+max_length = 69
+batch_size = 50
+def to_input(sentence, character_map, dictionary_size):
+    
+    sentence = np.array([character_map[char] for char in sentence])
+    one_hot = np.zeros((len(sentence), dictionary_size))
+    one_hot[np.arange(len(sentence)), sentence] = 1
+    return one_hot
+def batch(sentences, labels, start, batch_size):
+    
+    if start + batch_size < len(sentences):
+        
+        inputs = [to_input(sentence, character_map, dictionary_size) for sentence in sentences[start: start + batch_size ]]
+        labels = [label for label in labels[start: start + batch_size ]]
+        start = start + batch_size
+    
+    else:
+        
+        inputs = [to_input(sentence, character_map, dictionary_size) for sentence in sentences[start:]]
+        labels = [label for label in labels[start:]]
+        start = 0
+        
+    return np.array(inputs), np.array(labels) , start
+
+def test_batch(sentences, labels):
+    
+        
+    inputs = [to_input(sentence, character_map, dictionary_size) for sentence in sentences]
+    labels = [label for label in labels]
+
+    return np.array(inputs), np.array(labels)
+
+inputs = tf.placeholder(tf.float32, [None, max_length, dictionary_size])
+labels = tf.placeholder(tf.int64, [None])
+hidden_size = 512
+cell = tf.contrib.rnn.LSTMCell(hidden_size, state_is_tuple = True)
+initial_state = tf.contrib.rnn.LSTMStateTuple(tf.placeholder(tf.float32, [None, hidden_size], name = "c_init"), 
+                                             tf.placeholder(tf.float32, [None, hidden_size], name = "h_init"))
+with tf.variable_scope("softmax") as softmax:
+    
+    W_h = tf.get_variable("Wh", 
+        shape = [hidden_size, 2], 
+        dtype = tf.float32,
+        initializer = tf.random_normal_initializer(.1)
+    )
+
+    b_h = tf.get_variable("bh", 
+        shape = 2,
+        dtype = tf.float32,
+        initializer = tf.ones_initializer()
+    )
+    
+outputs, state = tf.nn.dynamic_rnn(cell, inputs, initial_state = initial_state)
+
+logits = tf.matmul(outputs[:,-1, :], W_h) + b_h
+
+loss = tf.reduce_mean(
+    tf.nn.sparse_softmax_cross_entropy_with_logits(
+        labels = labels, logits = logits
+    )
+)
+
+accuracy = tf.reduce_mean(
+    tf.cast(tf.equal(
+        tf.argmax(logits, 1),
+        tf.cast(labels, tf.int64)
+    ), 
+    tf.float32)
+)
+
+optimizer = tf.train.AdamOptimizer(.001)
+
+train_step = optimizer.minimize(loss)
